c5304f6f1db31833dc354be6ae0f9ba41f0988ee
Austin Rasmussen <ar@austinrasmussen.com>
2023-09-14 07:59:38 -0500

    Pull the recorder DB URL as default

--- custom_components/sql_template/config_flow.py
+++ custom_components/sql_template/config_flow.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import Session, scoped_session, sessionmaker
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.components.recorder import CONF_DB_URL, DEFAULT_DB_FILE, DEFAULT_URL
+from homeassistant.components.recorder import CONF_DB_URL, get_instance
 from homeassistant.const import CONF_NAME, CONF_UNIT_OF_MEASUREMENT, CONF_VALUE_TEMPLATE
 from homeassistant.core import callback, HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
@@ -90,12 +90,11 @@ class SQLConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> FlowResult:
         """Handle the user step."""
         errors = {}
-        db_url_default = DEFAULT_URL.format(
-            hass_config_path=self.hass.config.path(DEFAULT_DB_FILE)
-        )
+        db_url_default = get_instance(self.hass).db_url
 
         if user_input is not None:
             db_url = user_input.get(CONF_DB_URL, db_url_default)
+            _LOGGER.info(f"Using DBURL {db_url}")
             query = user_input[CONF_QUERY]
             column = user_input[CONF_COLUMN_NAME]
             uom = user_input.get(CONF_UNIT_OF_MEASUREMENT)
@@ -145,9 +144,7 @@ class SQLOptionsFlowHandler(config_entries.OptionsFlow):
     ) -> FlowResult:
         """Manage SQL options."""
         errors = {}
-        db_url_default = DEFAULT_URL.format(
-            hass_config_path=self.hass.config.path(DEFAULT_DB_FILE)
-        )
+        db_url_default = get_instance(self.hass).db_url
 
         if user_input is not None:
             db_url = user_input.get(CONF_DB_URL, db_url_default)
